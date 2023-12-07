9eaa9014054eea543e2cc66a1194a3cc05e7711d
Jimmy Shong <jimmysh341@gmail.com>
2023-12-07 09:47:23 -0500

    some changes

--- timeline.js
+++ timeline.js
@@ -1,19 +1,27 @@
-// Add this code inside the timeline.js file
-
-document.addEventListener('DOMContentLoaded', function () {
-    const timelineContainer = document.getElementById('timeline-container');
-    const timelineItems = timelineContainer.querySelectorAll('.timeline-item');
-
-    timelineItems.forEach(item => {
-        item.addEventListener('click', function () {
-            scrollToYear(item.getAttribute('data-year'));
-        });
-    });
-
-    function scrollToYear(year) {
-        const targetYear = document.querySelector(`[data-year="${year}"]`);
-        if (targetYear) {
-            targetYear.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
+(function () {
+    const items = document.querySelectorAll(".timeline-section li");
+    function isElementInViewport(el) {
+      let rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
     }
-});
+  
+    function slideIn() {
+      for (let i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("slide-in");
+        } else {
+          items[i].classList.remove("slide-in");
+        }
+      }
+    }
+  
+    window.addEventListener("load", slideIn);
+    window.addEventListener("scroll", slideIn);
+    window.addEventListener("resize", slideIn);
+  })();
