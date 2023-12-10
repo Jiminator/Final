564664cb34b1c501033813bbf00839e9714b540e
Jimmy Shong <jimmysh341@gmail.com>
2023-12-10 04:41:30 -0500

    changes

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Select all timeline items
-    const timelineItems = document.querySelectorAll(".Timeline__item");
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+  	}
+
+  	sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  } else {
+  	
+  	sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  }
   
-    // Hide all timeline items initially
-    timelineItems.forEach((item) => {
-      item.style.opacity = 0;
-      item.style.transition = "opacity 0.6s ease-out";
-    });
-  
-    // Intersection Observer Callback
-    const fadeInOnScroll = (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.style.opacity = 1;
-        }
-      });
-    };
-  
-    // Intersection Observer Options
-    const options = {
-      root: null, // uses the viewport as root
-      rootMargin: "0px",
-      threshold: 0.1, // 10% of the item should be visible
-    };
-  
-    // Creating Intersection Observer
-    const observer = new IntersectionObserver(fadeInOnScroll, options);
-  
-    // Observing each timeline item
-    timelineItems.forEach((item) => {
-      observer.observe(item);
-    });
-  });
+  sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+
+});
