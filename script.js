1a5ab7fab9c3ae231b9c4caf74de2976331b03ac
Jimmy Shong <jimmysh341@gmail.com>
2023-12-12 06:11:12 -0500

    changes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', function () {
-	// Initialize ScrollReveal when the DOM is fully loaded
-	ScrollReveal().reveal('.timeline-item', {
-	  delay: 200,
-	  distance: '20px',
-	  origin: 'bottom',
-	  easing: 'ease-in-out',
-	  reset: true,
-	  viewFactor: 0.2
-	});
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+// 	ScrollReveal().reveal('.headline');
+	// const sr = ScrollReveal();
+	// const revealConfig = {
+	// 	origin: 'bottom',
+	// 	distance: '20px',
+	// 	duration: 1000,
+	// 	delay: 200,
+	// 	easing: 'cubic-bezier(0.5, 0, 0, 1)',
+	// };
+	// sr.reveal('.timeline-content', revealConfig);
+// });
   
+// ScrollReveal().reveal('.headline');
